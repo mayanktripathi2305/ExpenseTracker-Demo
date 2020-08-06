f7a0fa79c4fe09d7901e7098ae98f115857ab3b7
Mayank <mayanktripathi2305@gmail.com>
2020-08-06 23:21:45 +0530

    Added Bootstrap Navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,40 @@
 import React from "react";
-import { Container, Row, Col } from "reactstrap";
+import "./Expense.css";
+
+import { container, nav, navbar } from "bootstrap";
+
+// import styled from "styled-components";
+// const Navitem = styled.reactstrap.NavItem`
+//   font-size: 2.5em;
+// `;
 
 const Header = () => {
   return (
-    <Container className="text-center" style={{ marginTop: 100 }}>
-      <Row>
-        <Col sm={{ size: 6, offset: 3 }}>
+    <>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">
+          Expense Tracker
+        </a>
+        <div className="navbar-nav ml-auto">
+          <a className="nav-link" href="#" style={{ fontSize: 20 }}>
+            About
+          </a>
+          <button
+            class="btn btn-primary mx-4"
+            type="button"
+            style={{ fontSize: 20 }}
+          >
+            Contact Us
+          </button>
+        </div>
+      </nav>
+
+      <div className="container-md" style={{ marginTop: 100 }}>
+        <div className="row justify-content-center">
           <h2>Expense Tracker</h2>
-        </Col>
-      </Row>
-    </Container>
+        </div>
+      </div>
+    </>
   );
 };
 
