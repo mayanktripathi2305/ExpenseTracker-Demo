6a18c51b9ad6d9afd249c1aaacc1bd29a84487dc
Mayank <mayanktripathi2305@gmail.com>
2020-08-07 01:08:18 +0530

    Added Router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./Expense.css";
 
 import { container, nav, navbar } from "bootstrap";
 
@@ -11,24 +10,6 @@ import { container, nav, navbar } from "bootstrap";
 const Header = () => {
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">
-          Expense Tracker
-        </a>
-        <div className="navbar-nav ml-auto">
-          <a className="nav-link" href="#" style={{ fontSize: 20 }}>
-            About
-          </a>
-          <button
-            class="btn btn-primary mx-4"
-            type="button"
-            style={{ fontSize: 20 }}
-          >
-            Contact Us
-          </button>
-        </div>
-      </nav>
-
       <div className="container-md" style={{ marginTop: 100 }}>
         <div className="row justify-content-center">
           <h2>Expense Tracker</h2>
