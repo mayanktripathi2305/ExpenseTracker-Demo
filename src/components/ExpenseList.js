9a6b84c45ba45a709770636f05a2a654839a5bfe
Mayank <mayanktripathi2305@gmail.com>
2020-08-10 00:17:20 +0530

    Reorganized

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,30 +1,18 @@
 import React from "react";
-import { ListGroup, ListGroupItem, Container } from "reactstrap";
 
 const ExpenseList = ({ expenses }) => {
   return (
     <>
       <div className="container-md" style={{ marginTop: 20 }}>
-        <div className="row">
-          <div className="col-md-6">
-            <ul className="list-group">
-              {expenses.map((expense) => (
-                <li className="list-group-item" key={expense.id}>
-                  {expense.name}
-                </li>
-              ))}
-            </ul>
-          </div>
-
-          <div className="col-md-6">
-            <ul className="list-group">
-              {expenses.map((expense) => (
-                <li className="list-group-item" key={expense.id}>
-                  Rs. {expense.amount}
-                </li>
-              ))}
-            </ul>
-          </div>
+        <div className="row justify-content-center">
+          <h2>Expenses</h2>
+          <ul className="list-group">
+            {expenses.map((expense) => (
+              <li className="list-group-item" key={expense.ID}>
+                {expense.name} Rs. {expense.amount}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </>
