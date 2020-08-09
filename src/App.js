8e7a1ccbea511e2703ee535e2263aaefd19d9a67
Mayank <mayanktripathi2305@gmail.com>
2020-08-10 00:21:26 +0530

    Commented the Protected Router, Login component - Login via google will be implemented as part of MERN stack Expense Tracler

--- src/App.js
+++ src/App.js
@@ -9,17 +9,25 @@ import AddExpense from "./components/AddExpense";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Nav from "./components/Nav";
-
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
 const ALL_EXPENCES = localStorage.getItem("expenses")
   ? JSON.parse(localStorage.getItem("expenses"))
   : [];
 
+// const ALL_EXPENCES = [
+//   { ID: 1, name: "abbb", amount: 22 },
+//   { ID: 2, name: "bbb", amount: 32 },
+// ];
+
 function App() {
   const [expenses, setExpenses] = useState(ALL_EXPENCES);
 
-  const [id, setID] = useState(1);
+  const [ID, setID] = useState(1);
   const [name, setName] = useState("");
   const [amount, setAmount] = useState(0);
+  // const [accessToken, setAccessToken] = useState("");
 
   const AddName = (e) => {
     setName(e.target.value);
@@ -32,10 +40,10 @@ function App() {
   const SubmitForm = (e) => {
     e.preventDefault();
 
-    setID((prevID) => prevID + 1);
+    setID(ID + 1);
 
     if (name !== "" && amount > 0) {
-      const newexpense = { id, name, amount };
+      const newexpense = { ID, name, amount };
 
       setExpenses([...expenses, newexpense]);
       setName("");
@@ -49,20 +57,50 @@ function App() {
     setExpenses([]);
   };
 
+  // const loginSuccess = () => {
+  //   setAccessToken();
+  // };
+
   useEffect(() => {
     localStorage.setItem("expenses", JSON.stringify(expenses));
   }, [expenses]);
+
+  // const allProps = {
+  //   ID,
+  //   expenses,
+  //   name,
+  //   amount,
+  //   AddName,
+  //   AddAmount,
+  //   SubmitForm,
+  //   DeleteAll,
+  // };
+
   return (
     <div>
       <Router>
         <Nav />
 
         <Switch>
-          <Route path="/" exact component={Header}></Route>
+          {/* <Route
+            path="/"
+            exact
+            render={(props) => (
+              <>
+                <Header />
+                <Login />
+              </>
+            )}
+          ></Route> */}
+          <Header />
+          {/* <ProtectedRoute
+            path="/Dashboard"
+            component={Dashboard}
+          ></ProtectedRoute> */}
           <Route path="/About" component={About}></Route>
           <Route path="/Contact" component={Contact}></Route>
         </Switch>
-        <ExpenseTotal expense={expenses} />
+        <ExpenseTotal expenses={expenses} />
         <ExpenseList expenses={expenses} />
         <AddExpense
           name={name}
