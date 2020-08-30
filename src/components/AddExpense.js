8b5af3eaaad4a4e326b17fa89cd2036023e274ce
Mayank <mayanktripathi2305@gmail.com>
2020-08-30 09:09:24 +0530

    Added  Income Array

--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -4,16 +4,29 @@ import { Form, FormGroup, Label, Input } from "reactstrap";
 const AddExpense = ({
   name,
   amount,
+  income,
   expenses_Type,
   AddName,
   AddAmount,
-  AddExpense,
+  AddIncome,
+  AddExpenseType,
   SubmitForm,
+  SubmitIncome,
   DeleteAll,
 }) => {
   return (
     <div className="container" style={{ marginTop: 100 }}>
       <div className="row justify-content-center">
+        <div className="col-md-auto">
+          <button
+            type="button"
+            className="ManageExpense"
+            data-toggle="modal"
+            data-target="#staticBackdrop1"
+          >
+            Add Income
+          </button>
+        </div>
         <div className="col-md-auto">
           <button
             type="button"
@@ -56,7 +69,7 @@ const AddExpense = ({
               </button>
             </div>
             <div className="modal-body">
-              <FormGroup>
+              {/* <FormGroup>
                 <div className="dropdown">
                   <button
                     className="btn btn-warning dropdown-toggle dropdown-toggle-split"
@@ -75,16 +88,16 @@ const AddExpense = ({
                     {expenses_Type.map((expenseType) => (
                       <a
                         className="dropdown-item"
-                        key={expenseType.expenseType}
-                        value={expenseType.expenseType}
-                        onChange={AddExpense}
+                        key={expenseType}
+                        value={expenseType}
+                        onChange={AddExpenseType}
                       >
                         {expenseType}
                       </a>
                     ))}
                   </div>
                 </div>
-              </FormGroup>
+              </FormGroup> */}
               <FormGroup>
                 <Label for="Expense Type">Add Expense For</Label>
                 <Input
@@ -116,6 +129,64 @@ const AddExpense = ({
                 type="button"
                 className="btn btn-primary"
                 onClick={SubmitForm}
+              >
+                Add
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* For Adding Income*/}
+
+      <div
+        className="modal fade"
+        id="staticBackdrop1"
+        data-backdrop="static"
+        data-keyboard="false"
+        tabIndex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="staticBackdropLabel">
+                Add Income
+              </h5>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <FormGroup>
+                <Label for="Expense Type">Add Income</Label>
+                <Input
+                  type="number"
+                  value={income}
+                  placeholder="e.g 1000"
+                  onChange={AddIncome}
+                />
+              </FormGroup>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-dismiss="modal"
+              >
+                Cancel
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                data-dismiss="modal"
+                onClick={SubmitIncome}
               >
                 Add
               </button>
